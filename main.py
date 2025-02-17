5a01fe1848142d8d052e6a2441523785ab509f35
flotr18 <trouilletflorent@outlook.com>
2025-02-17 15:17:44 +0100

    Def Intro created

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-import random as rd 
-def hello():
+import random as rd
+
+def intro():
+   Serve = 0
    print("Hello ! Welcome to Tennis")
+   ChoiceServe = rd.randint(1,2)
+   if ChoiceServe == 1:
+      
+    SelectP1 = input("P1, Serve or Receive ? S/R: ")
+      
+    if SelectP1 == "R":
+        "Fine, you will receive!"
+        Serve == 1
+        print("P2 to Serve! ")
+    else:
+        Serve == 2
+        print("P1 to Serve! ")
+
+   else:
+      SelectP1 = input("P2, Serve or Receive ? S/R: ")
+      if SelectP1 == "R":
+         "Fine, you will receive!"
+         Serve == 1
+         print("P1 to Serve! ")
+      else:
+         Serve == 2
+         print("P2 to Serve! ")
+
+intro()
+
+def Game():
+   ScoreP1 = 0
+   ScoreP2 = 0
+   Point = rd.randint(1,2)
+   
+
    
