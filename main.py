77f416a85dbfca699c6889a2f1ccc5c8c1ab41da
flotr18 <trouilletflorent@outlook.com>
2025-02-17 22:06:25 +0100

    Reset INtro

--- main.py
+++ main.py
@@ -1,48 +1,22 @@
 import random as rd
 
 def intro():
-   Serve = 0
-   print("Hello ! Welcome to Tennis")
-   ChoiceServe = rd.randint(1,2)
-   if ChoiceServe == 1:
-      
-    SelectP1 = input("P1, Serve or Receive ? S/R: ")
-      
-    if SelectP1 == "R":
-        "Fine, you will receive!"
-        Serve == 1
-        print("P2 to Serve! ")
+    Serve = rd.randint(0,1)
+    if Serve == 0:
+        Choice = input("P1 won ! Servce or Receive? S/R ")
+        if Choice == "R":
+            print("P2 to Serve")
+        else:
+            print("P1 to Serve")
+
     else:
-        Serve == 2
-        print("P1 to Serve! ")
+        Choice = input("P2 won ! Servce or Receive? S/R ")
+        if Choice == "R":
+            print("P1 to Serve")
+        else:
+            print("P2 to Serve")
 
-   else:
-      SelectP1 = input("P2, Serve or Receive ? S/R: ")
-      if SelectP1 == "R":
-         "Fine, you will receive!"
-         Serve == 1
-         print("P1 to Serve! ")
-      else:
-         Serve == 2
-         print("P2 to Serve! ")
 
+    
 intro()
-
-def Game():
-   ScoreP1 = 0
-   ScoreP2 = 0
-   while ScoreP1 != 4 or ScoreP2 != 4: 
-        Point = rd.randint(1,2)
-        if Point == 1: 
-            ScoreP1 += 1
-        else :
-           ScoreP2 += 1
         
-        if ScoreP1 == 4 : 
-            print("P1 wins the game!")
-        elif ScoreP2 == 4:
-            print("P2 wins the game!")
-      
-
-
-   
