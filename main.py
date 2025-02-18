74400c39a11f207ead65d2322036865c00b57e31
flotr18 <trouilletflorent@outlook.com>
2025-02-18 11:44:10 +0100

    Add Score Evol

--- main.py
+++ main.py
@@ -20,4 +20,35 @@ def intro():
     
 intro()
 
+def ScoreEvol():
+    scores = ["0","15","30","40","A"]
+    ScoreP1 = 0
+    ScoreP2 = 0
+
+    while True:
+        PointWinner = rd.randint(0,1)
+        if PointWinner == 0:
+            ScoreP1 +=1
+        else:
+            ScoreP2 +=2
+
+        if ScoreP1 >= 4 and ScoreP1 - ScoreP2 >= 2:
+            print("P1 wins the game!")
+
+        elif ScoreP2 >= 4 and ScoreP2 - ScoreP1 >= 2:
+            print("P2 wins the game!")
+        elif ScoreP1 >= 3 and ScoreP2 >= 3:
+            if ScoreP1 == ScoreP2:
+                print("Deuce")
+            elif ScoreP1 > ScoreP1:
+                print("Avantage P1")
+            else:
+                print("Avantage P2")
+        else:
+            print(f"Score P1 - {scores[ScoreP1]} Score P2 - {scores[ScoreP2]}")
+
+        
+
+    
+ScoreEvol()
 
