12d7feefd1582990ea591ce2e5d8d3b2fec0aa39
flotr18 <trouilletflorent@outlook.com>
2025-02-18 14:19:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,32 +20,36 @@ def intro():
     
 intro()
 
+
 def ScoreEvol():
     scores = ["0","15","30","40","A"]
     ScoreP1 = 0
     ScoreP2 = 0
+    GameP1 = 0
+    GameP2 = 0
+    PointWinner = rd.randint(0,1)
+    if PointWinner == 0:
+        ScoreP1 +=1
+    else:
+        ScoreP2 +=2
 
-    while True:
-        PointWinner = rd.randint(0,1)
-        if PointWinner == 0:
-            ScoreP1 +=1
+    if ScoreP1 >= 4 and ScoreP1 - ScoreP2 >= 2:
+        print("P1 wins the game!")
+        GameP1 += 1
+
+    elif ScoreP2 >= 4 and ScoreP2 - ScoreP1 >= 2:
+        print("P2 wins the game!")
+        GameP2 += 1
+        
+    elif ScoreP1 >= 3 and ScoreP2 >= 3:
+        if ScoreP1 == ScoreP2:
+            print("Deuce")
+        elif ScoreP1 > ScoreP1:
+            print("Avantage P1")
         else:
-            ScoreP2 +=2
-
-        if ScoreP1 >= 4 and ScoreP1 - ScoreP2 >= 2:
-            print("P1 wins the game!")
-
-        elif ScoreP2 >= 4 and ScoreP2 - ScoreP1 >= 2:
-            print("P2 wins the game!")
-        elif ScoreP1 >= 3 and ScoreP2 >= 3:
-            if ScoreP1 == ScoreP2:
-                print("Deuce")
-            elif ScoreP1 > ScoreP1:
-                print("Avantage P1")
-            else:
-                print("Avantage P2")
-        else:
-            print(f"Score P1 - {scores[ScoreP1]} Score P2 - {scores[ScoreP2]}")
+            print("Avantage P2")
+    else:
+        print(f"Score P1 - {scores[ScoreP1]} Score P2 - {scores[ScoreP2]}")
 
         
 
